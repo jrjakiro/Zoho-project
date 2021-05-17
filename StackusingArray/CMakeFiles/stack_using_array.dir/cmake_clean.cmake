a76a70dfe05bbf9b6cf135ac2471e47212657752
jrjakiro <jrjakiro2001@gmail.com>
2021-05-17 21:45:47 +0530

    Updated stack using array as per instructions

---
+++ StackusingArray/CMakeFiles/stack_using_array.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/stack_using_array.dir/main.cpp.o"
+  "CMakeFiles/stack_using_array.dir/main.cpp.o.d"
+  "stack_using_array"
+  "stack_using_array.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/stack_using_array.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
