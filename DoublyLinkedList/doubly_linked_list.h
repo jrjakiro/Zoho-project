f8bd68028e8ce682086d6f5d209315b56d61d3c9
jrjakiro <jrjakiro2001@gmail.com>
2021-05-22 13:17:18 +0530

    Updated the linkedlist for int, float, char, double and long datatypes

--- DoublyLinkedList/doubly_linked_list.h
+++ DoublyLinkedList/doubly_linked_list.h
@@ -1,42 +1,45 @@
 #include <iostream>
 using namespace std;
 // Node Creation
+template <class T>
 class node
 {
 public:
-    int data;
+    T data;
     node *next;
     node *prev;
-    node(node *x, int value, node *y)
-    {
-        data = value;
-        prev = x;
-        next = y;
-    };
+    node(node *x, T value, node *y);
 };
-
+template <typename T>
+node<T>::node(node *x, T value, node *y)
+{
+    data = value;
+    prev = x;
+    next = y;
+};
+template <typename T>
 class doubly_linked_list
 { //class
-    node *head = nullptr;
-    node *tail = nullptr;
+    node<T> *head = nullptr;
+    node<T> *tail = nullptr;
     int length = 0;
     //for finding node
-    node *find_node(int location);
+    node<T> *find_node(int location);
 
 public:
     // insertion of value at front & back
-    void push_front(int value);
-    void push_back(int value);
+    void push_front(T value);
+    void push_back(T value);
 
     //for inserting at location
-    void insert(int value, int location);
+    void insert(T value, int location);
 
     //for removing values at front and back
-    int remove_front();
-    int remove_back();
+    T remove_front();
+    T remove_back();
 
     //for removing at a location and from a range
-    int remove(int location);
+    T remove(int location);
     void remove(int start, int end);
 
     //for finding the size
@@ -52,9 +55,10 @@ public:
     void clear();
 };
 //for  finding node at a position
-node *doubly_linked_list::find_node(int location)
+template <typename T>
+node<T> *doubly_linked_list<T>::find_node(int location)
 {
-    node *temp = head;
+    node<T> *temp = head;
     int count = 1;
     //iterating to insert at a position
     while (count != location)
@@ -68,17 +72,20 @@ node *doubly_linked_list::find_node(int location)
 }
 
 //adding element at the front
-void doubly_linked_list::push_front(int value)
+template <typename T>
+void doubly_linked_list<T>::push_front(T value)
 {
     insert(value, 0);
 }
 //add the element at the back
-void doubly_linked_list::push_back(int value)
+template <typename T>
+void doubly_linked_list<T>::push_back(T value)
 {
-    insert(value, length - 1);
+    insert(value, length);
 }
 //for inserting at a location
-void doubly_linked_list::insert(int value, int location)
+template <typename T>
+void doubly_linked_list<T>::insert(T value, int location)
 {
     if (location > length || location < 0)
     {
@@ -89,7 +96,7 @@ void doubly_linked_list::insert(int value, int location)
     if (location == 0)
     {
         //new node creation
-        node *new_node = new node(nullptr, value, head);
+        node<T> *new_node = new node<T>(nullptr, value, head);
 
         //if head node is not empty then prev of head node will be assigned to the new node
         if (head != nullptr)
@@ -109,7 +116,7 @@ void doubly_linked_list::insert(int value, int location)
     if (location == length)
     {
         //new node creation
-        node *new_node = new node(tail, value, nullptr);
+        node<T> *new_node = new node<T>(tail, value, nullptr);
 
         //if the list is not empty then tail node is not empty then next of tail node will be assigned to the new node
         if (tail != nullptr)
@@ -127,12 +134,12 @@ void doubly_linked_list::insert(int value, int location)
     }
 
     //created temporary nodes which will point to previous and next node
-    node *temp1 = find_node(location);
+    node<T> *temp1 = find_node(location);
     //new node has to be inserted after the temp1 node then temp1 is the previous node
     //temp2 node will be next node
-    node *temp2 = temp1->next;
+    node<T> *temp2 = temp1->next;
     //as the new node is inserted between two temp nodes,
-    node *new_node = new node(temp1, value, temp2);
+    node<T> *new_node = new node<T>(temp1, value, temp2);
     //assigning values for the two before and after node.
     temp1->next = new_node;
     temp2->prev = new_node;
@@ -141,17 +148,20 @@ void doubly_linked_list::insert(int value, int location)
 }
 
 //for removing a node at front in the list
-int doubly_linked_list::remove_front()
+template <typename T>
+T doubly_linked_list<T>::remove_front()
 {
     return remove(0);
 }
 //for removing last item in the list
-int doubly_linked_list::remove_back()
+template <typename T>
+T doubly_linked_list<T>::remove_back()
 {
     return remove(length - 1);
 }
 //for removing node at a position
-int doubly_linked_list::remove(int location)
+template <typename T>
+T doubly_linked_list<T>::remove(int location)
 {
     if (location >= length || location < 0)
     {
@@ -169,7 +179,7 @@ int doubly_linked_list::remove(int location)
         //if the list has only one item
         if (head == tail)
         {
-            int value = head->data;
+            T value = head->data;
             delete (head);
             head = tail = nullptr;
             length--;
@@ -178,9 +188,9 @@ int doubly_linked_list::remove(int location)
         //if the list has two or more item
 
         // creating a temporary variable to store head node
-        node *temp = head;
+        node<T> *temp = head;
         //assigning the data to value in order to return
-        int value = temp->data;
+        T value = temp->data;
         //assigning the second nodes's prev pointer to null and change change head pointing to it
         temp->next->prev = nullptr;
         head = temp->next;
@@ -199,7 +209,7 @@ int doubly_linked_list::remove(int location)
         }
         if (head == tail)
         {
-            int value;
+            T value;
             delete (head);
             head = tail = nullptr;
             length--;
@@ -207,9 +217,9 @@ int doubly_linked_list::remove(int location)
         }
         //if the list has two or more item
         // creating a temporary variable to store tail node
-        node *temp = tail;
+        node<T> *temp = tail;
         //assigning the data to value in order to return
-        int value = temp->data;
+        T value = temp->data;
         //assigning the last second nodes's next pointer to null and change tail pointing to it
         temp->prev->next = nullptr;
         tail = temp->prev;
@@ -220,7 +230,7 @@ int doubly_linked_list::remove(int location)
         return value;
     }
 
-    node *temp = find_node(location);
+    node<T> *temp = find_node(location);
     (temp->next)->prev = temp->prev;
     (temp->prev)->next = temp->next;
     //deleting the memory occupied
@@ -228,8 +238,9 @@ int doubly_linked_list::remove(int location)
     length--;
     return 0;
 }
+template <typename T>
 //for deleting the nodes between from and two
-void doubly_linked_list::remove(int start, int end)
+void doubly_linked_list<T>::remove(int start, int end)
 {
     //checking whether the range is able to delete or not
     if (start < 0 || start > end || end > length)
@@ -237,7 +248,7 @@ void doubly_linked_list::remove(int start, int end)
         cout << "Invalid Range to Remove" << endl;
         return;
     }
-    node *current = head;
+    node<T> *current = head;
     int count = start;
     while (start <= end)
     {
@@ -247,7 +258,8 @@ void doubly_linked_list::remove(int start, int end)
 }
 
 //clearing all the contents
-void doubly_linked_list::clear()
+template <typename T>
+void doubly_linked_list<T>::clear()
 {
     //iterate till the last node
     while (head != nullptr)
@@ -258,10 +270,11 @@ void doubly_linked_list::clear()
 }
 
 //prints the values in list
-void doubly_linked_list::print_values()
+template <class T>
+void doubly_linked_list<T>::print_values()
 {
     cout << "\n";
-    node *x = head;
+    node<T> *x = head;
     if (head == nullptr)
     {
         cout << "\nNo Items in the list and it is empty" << endl;
@@ -276,13 +289,15 @@ void doubly_linked_list::print_values()
 };
 
 //to find the size
-size_t doubly_linked_list::size()
+template <typename T>
+size_t doubly_linked_list<T>::size()
 {
     return length;
 }
 
 //to check for empty or not
-bool doubly_linked_list::is_empty()
+template <typename T>
+bool doubly_linked_list<T>::is_empty()
 {
     return (length == 0);
 }
--- DoublyLinkedList/main.cpp
+++ DoublyLinkedList/main.cpp
@@ -1,55 +1,59 @@
 
 #include "doubly_linked_list.h"
+#include "string.h"
 using namespace std;
 int main() //drivercode
 {
-    doubly_linked_list dll;
+    doubly_linked_list<int> dli;
+    doubly_linked_list<float> dlf;
+    doubly_linked_list<char> dlc;
+    doubly_linked_list<string> dls;
+    doubly_linked_list<long> dll;
+    doubly_linked_list<double> dld;
+    dls.push_front("HELLO");
+    dls.print_values();
 
-    dll.push_front(10); //push_fronting values at the front
-    dll.push_front(25);
-    dll.push_front(10); //push_fronting values at the front
-    dll.push_front(25);
-    dll.push_front(10); //push_fronting values at the front
-    dll.push_front(25);
-    dll.push_front(10); //push_fronting values at the front
-    dll.push_front(25);
-    dll.push_front(10); //push_fronting values at the front
-    dll.push_front(25);
+    dli.push_front(10); //push_fronting values at the front
+    dli.push_front(25);
+    dli.push_front(10); //push_fronting values at the front
+    dli.push_front(25);
+    dli.push_front(10); //push_fronting values at the front
+    dli.push_front(25);
+    dli.push_front(10); //push_fronting values at the front
+    dli.push_front(25);
+    dli.push_front(10); //push_fronting values at the front
+    dli.push_front(25);
 
-    dll.push_back(24); //push_fronting elements at the back
+    dli.push_back(24); //push_fronting elements at the back
 
-    dll.insert(33,1); //Inseting at a location
+    dli.insert(33, 1); //Inseting at a location
 
     cout << "Before Deletion,Values are ";
-    dll.print_values(); //printing all values before deletion
+    dli.print_values(); //printing all values before deletion
 
-    cout << "\nAfter removing the first value "<<dll.remove_front()<<endl;
-    dll.print_values();
+    cout << "\nAfter removing the first value " << dli.remove_front() << endl;
+    dli.print_values();
 
-    cout << "\nAfter removing the last value "<<dll.remove_back()<<endl;
-    dll.print_values();
+    cout << "\nAfter removing the last value " << dli.remove_back() << endl;
+    dli.print_values();
 
-    dll.remove(2);
+    dli.remove(2);
     cout << "\nAfter removing the second value ";
-    dll.print_values();
+    dli.print_values();
 
-
-    dll.remove(3,5);
+    dli.remove(3, 5);
     cout << "\nAfter removing the 3rd-5th  value ";
-    dll.print_values();
+    dli.print_values();
 
-    cout << "\nList Empty : " << dll.is_empty()<<"\n"; //checking whether the list is empty or not return 1 if it is empty
+    cout << "\nList Empty : " << dli.is_empty() << "\n"; //checking whether the list is empty or not return 1 if it is empty
 
     cout << "\nAfter Deletion, Values are "; //Printing the values after deletion
-    dll.print_values();
+    dli.print_values();
 
     //clearing the list
-    dll.clear();
-    cout<< "\nAfter Clearing size of list is "<<dll.size();
-    dll.print_values();
+    dli.clear();
+    cout << "\nAfter Clearing size of list is " << dli.size();
+    dli.print_values();
 
-    cout << "\nList Empty : " << dll.is_empty()<<"\n"; //checking whether the list is empty or not return 1 if it is empty
-
-
-
+    cout << "\nList Empty : " << dli.is_empty() << "\n"; //checking whether the list is empty or not return 1 if it is empty
 }
